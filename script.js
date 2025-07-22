8c590749c61d9b0e8ed8261f7fc0f1886c9fdb87
Tanveer Abbas <134228033+Luyce13@users.noreply.github.com>
2025-07-22 12:47:14 +0500

    version 2

--- script.js
+++ script.js
@@ -1,124 +1,60 @@
 "use strict";
-const players = document.querySelectorAll(".player");
-const btnRoll = document.querySelector(".btn--roll");
-const btnHold = document.querySelector(".btn--hold");
-const btnReset = document.querySelector(".btn--new");
-const scoresCurrent = document.querySelectorAll(".current-score");
-const scoresTotal = document.querySelectorAll(".score");
-const dice = document.querySelector(".dice");
+const playerEls = document.querySelectorAll(".player");
+const currentEls = document.querySelectorAll(".current-score");
+const scoreEls = document.querySelectorAll(".score");
+const diceEl = document.querySelector(".dice");
+const btnNewEl = document.querySelector(".btn--new");
+const btnRollEl = document.querySelector(".btn--roll");
+const btnHoldEl = document.querySelector(".btn--hold");
 
-function switchPlayer() {
-    if (players[0].classList.contains("player--active")) {
-        players[0].classList.remove("player--active");
-        players[1].classList.add("player--active");
-        playerActive = 2;
-    } else {
-        players[1].classList.remove("player--active");
-        players[0].classList.add("player--active");
-        playerActive = 1;
-    }
-}
-
-function rollDice() {
-    if (!gameOver) {
-        const randomNumber = Math.trunc(Math.random() * 6 + 1);
-        dice.setAttribute("src", `dice-${randomNumber}.png`);
-        dice.classList.remove("hidden");
-        if (randomNumber !== 1) {
-            playerActive === 1
-                ? scoreCurrentPlayer1 += randomNumber
-                : scoreCurrentPlayer2 += randomNumber;
-            updateCurrentScore(
-                playerActive === 1 ? scoreCurrentPlayer1 : scoreCurrentPlayer2,
-                playerActive,
-            );
-        } else {
-            playerActive === 1
-                ? scoreCurrentPlayer1 = 0
-                : scoreCurrentPlayer2 = 0;
-            updateCurrentScore(
-                playerActive === 1 ? scoreCurrentPlayer1 : scoreCurrentPlayer2,
-                playerActive,
-            );
-            switchPlayer();
-        }
-    }
-}
-
-function updateCurrentScore(n, p) {
-    if (p === 1) {
-        scoresCurrent[0].textContent = n;
-    } else {
-        scoresCurrent[1].textContent = n;
-    }
-}
-
-function updateTotalScore(n, p) {
-    if (p === 1) {
-        // scoreCurrentPlayer1 = n;
-        scoresTotal[0].textContent = n;
-    } else {
-        // scoreCurrentPlayer2 = n;
-        scoresTotal[1].textContent = n;
-    }
-}
-
-function holdScore() {
-    if (playerActive === 1) {
-        scoreTotalPlayer1 += scoreCurrentPlayer1;
-        scoresTotal[0].textContent = scoreTotalPlayer1;
-        scoreCurrentPlayer1 = 0;
-        updateCurrentScore(0, 1);
-        console.log(scoreTotalPlayer1, scoreTotalPlayer1 >= 100);
-        if (scoreTotalPlayer1 >= 100) {
-            players[0].classList.add("player--winner");
-            players[0].classList.remove("player--active");
-            gameOver = true;
-        } else switchPlayer();
-    } else {
-        scoreTotalPlayer2 += scoreCurrentPlayer2;
-        scoresTotal[1].textContent = scoreTotalPlayer2;
-        scoreCurrentPlayer2 = 0;
-        updateCurrentScore(0, 2);
-        console.log(scoreTotalPlayer2, scoreTotalPlayer2 >= 100);
-        if (scoreTotalPlayer2 >= 100) {
-            players[1].classList.add("player--winner");
-            players[1].classList.remove("player--active");
-            gameOver = true;
-        } else switchPlayer();
-    }
-}
-
-function reset() {
-    scoreCurrentPlayer1 = 0;
-    scoreCurrentPlayer2 = 0;
-    scoreTotalPlayer1 = 0;
-    scoreTotalPlayer2 = 0;
-    updateCurrentScore(0, 1);
-    updateCurrentScore(0, 2);
-    updateTotalScore(0, 1);
-    updateTotalScore(0, 2);
-    playerActive = 1;
-    if (!players[0].classList.contains("player--active")) {
-        players[0].classList.add("player--active");
-        players[1].classList.remove("player--active");
-    }
-    players[0].classList.contains("player--winner")
-        ? players[0].classList.remove("player--winner")
-        : players[1].classList.remove("player--winner");
-    dice.classList.add("hidden");
-    gameOver = false;
-}
-
-let scoreTotalPlayer1 = 0;
-let scoreTotalPlayer2 = 0;
-
-let scoreCurrentPlayer1 = 0;
-let scoreCurrentPlayer2 = 0;
-let playerActive = 1;
+let scores = [0, 0];
+let current = 0;
+let playerActive = 0;
 let gameOver = false;
 
-btnRoll.addEventListener("click", rollDice);
-btnHold.addEventListener("click", holdScore);
+const switchPlayer = function () {
+    currentEls[playerActive].textContent = 0;
+    playerEls.forEach((player) => player.classList.toggle("player--active"));
+    current = 0;
+    playerActive === 0 ? (playerActive = 1) : (playerActive = 0);
+};
 
-btnReset.addEventListener("click", reset);
+diceEl.classList.add("hidden");
+scoreEls.forEach((el) => (el.textContent = 0));
+currentEls.forEach((el) => (el.textContent = 0));
+
+btnRollEl.addEventListener("click", function () {
+    if (!gameOver) {
+        const diceValue = Math.trunc(Math.random() * 6) + 1;
+        diceEl.classList.remove("hidden");
+        diceEl.src = `dice-${diceValue}.png`;
+        if (diceValue !== 1) {
+            current += diceValue;
+            currentEls[playerActive].textContent = current;
+        } else switchPlayer();
+    }
+});
+
+btnHoldEl.addEventListener("click", function () {
+    if (!gameOver) {
+        scores[playerActive] += current;
+        scoreEls[playerActive].textContent = scores[playerActive];
+        if (scores[playerActive] >= 100) {
+            gameOver = true;
+            current = 0;
+            currentEls[playerActive].textContent = 0;
+            playerEls[playerActive].classList.add("player--winner");
+            playerEls[playerActive].classList.remove("player--active");
+        } else switchPlayer();
+    }
+});
+
+btnNewEl.addEventListener("click", function () {
+    playerEls[playerActive].classList.remove("player--winner");
+    playerActive = 0;
+    playerEls[playerActive].classList.add("player--active");
+    scoreEls.forEach((score) => (score.textContent = 0));
+    diceEl.classList.add("hidden");
+    scores = [0, 0];
+    gameOver = false;
+});
